49695cbf913cdc8f54246a3d534d29a23eaae824
myname21-meet <myname21@meet.mit.edu>
2019-12-19 11:53:27 -0500

    did somethimg

--- app.py
+++ app.py
@@ -6,9 +6,25 @@ def home():
     return render_template('home.html')
 
 @app.route('/study_image', methods = ['POST'])
+
+
+
 def study_image():
-    
-    image_url = request.form['url-input']
+        image_url = request.form['url-input']  
+        headers = {'Authorixation' : 'key < 3ec5e4ff7f814f37a5351e44c4c38685>'}
+        api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+        data ={"inputs": [
+              {
+                "data": {
+                 "image": {
+                  "url": 'image_url'
+                }
+              }
+          }
+       ]}
+
+response = request.post(api_url,headers=headers,data=jason.dumps(data))
+
     # At this point you have the image_url value from the front end
     # Your job now is to send this information to the Clarifai API
     # and read the result, make sure that you read and understand the
@@ -20,3 +36,7 @@ def study_image():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+    
